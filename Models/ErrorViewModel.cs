488ca11211a17005c8aa5872c637febd72a857bf
Marina <marina.khodova.06@mail.ru>
2026-05-04 19:21:34 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Documents.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
