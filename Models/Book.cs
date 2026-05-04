b29d5b0f71fee18664edb36730f50124a836cddd
SoslanKalkhiev <skalhiev@gmail.com>
2026-05-04 21:01:12 +0300

    мелочь
    Renamed IsReadOrNot to IsRead and changed Review to be nullable.

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,8 +8,8 @@
         public string Genre { get; set; } //Жанр
         public string Description { get; set; }
         //public DateOnly Date { get; set; } =???   //Дата (чего? Написания, добавления?)
-        public bool IsReadOrNot { get; set; } = false; //Прочтена или нет
-        public string Review { get; set; } = ""; //Пользовательские заметки, рецензия
+        public bool IsRead{ get; set; } = false; //Прочтена или нет
+        public string Review? { get; set; } ; //Пользовательские заметки, рецензия
 
         public Book(string name, string author, string genre, string description)
         {
