7eb710d96f0654c5fd30b36071bd86992e972686
Снорлакс Рождественский <xmassnorlax@outlook.com>
2026-05-23 02:43:39 +0300

    что то

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,12 +1,89 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Documents.Data;
+using Documents.Models;
 
 namespace Documents.Controllers
 {
     public class BookController : Controller
     {
-        public IActionResult Index()
+        IBookRepository bookRepository;
+
+        public BookController(IBookRepository bookRep)
         {
-            return View();
+            this.bookRepository = bookRep;
+        }
+
+        public IActionResult Index(Guid id)
+        {
+            var book = bookRepository.TryGetById(id);
+            if (book == null)
+                return NotFound();
+
+            return View(book);
+        }
+
+        public IActionResult Redact(Guid id)
+        {
+            var book = bookRepository.TryGetById(id);
+            if (book == null)
+                return NotFound();
+
+            return View(book);
+        }
+
+        public IActionResult ChangeReadStatus(Guid id)
+        {
+            var book = bookRepository.TryGetById(id);
+            if (book == null)
+                return NotFound();
+
+            book.IsRead = !book.IsRead;
+            SaveBooksToFile();
+
+            return RedirectToAction("Index", new { id = id });
+        }
+
+        public IActionResult Save(Guid Id, string Name, string Author, string Genre, string Description, bool IsRead, string? Review)
+        {
+            var book = bookRepository.TryGetById(Id);
+            if (book == null)
+                return NotFound();
+
+            book.Name = Name;
+            book.Author = Author;
+            book.Genre = Genre;
+            book.Description = Description;
+            book.IsRead = IsRead;
+
+            if (!string.IsNullOrEmpty(Review))
+                book.Review = Review;
+
+            SaveBooksToFile();
+
+            return RedirectToAction("Index", new { id = Id });
+        }
+
+        public IActionResult Delete(Guid Id)
+        {
+            var books = bookRepository.GetAll();
+            var bookToRemove = books.FirstOrDefault(b => b.Id == Id);
+
+            if (bookToRemove != null)
+            {
+                books.Remove(bookToRemove);
+                SaveBooksToFile();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private void SaveBooksToFile()
+        {
+            var books = bookRepository.GetAll();
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(books, options);
+            System.IO.File.WriteAllText("Data/books.json", json);
         }
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,65 @@
-using Documents.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using System.Text.Json;
+using Documents.Data;
+using Documents.Models;
 
 namespace Documents.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        ICollectionsRepository collectionsRepository;
+        IBookRepository bookRepository;
+        List<Collection> collections;
+        List<Book> books;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ICollectionsRepository collectRep, IBookRepository bookRep)
         {
-            _logger = logger;
+            this.collectionsRepository = collectRep;
+            this.bookRepository = bookRep;
+            collections = collectionsRepository.GetAll();
+            books = bookRepository.GetAll();
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(collections);
         }
 
-        public IActionResult Privacy()
+        public IActionResult AddNew(string Name, string Author, string Genre, string Description, bool IsRead, string? Review)
         {
-            return View();
-        }
+            Book book = new Book(Name, Author, Genre, Description);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (!string.IsNullOrEmpty(Review))
+                book.Review = Review;
+
+            book.IsRead = IsRead;
+
+            books.Add(book);
+
+            if (IsRead)
+            {
+                if (collections.Count > 1)
+                {
+                    collections[1].Books.Add(book.Id, book.Name);
+                }
+            }
+            else
+            {
+                if (collections.Count > 0)
+                {
+                    collections[0].Books.Add(book.Id, book.Name);
+                }
+            }
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+
+            string newbook = JsonSerializer.Serialize(books, options);
+            string newcoll = JsonSerializer.Serialize(collections, options);
+
+            System.IO.File.WriteAllText("Data/books.json", newbook);
+            System.IO.File.WriteAllText("Data/collections.json", newcoll);
+
+            return RedirectToAction("Index");
         }
     }
 }
