c7e00c245b546501e752f960b8e60cb1b661a6b3
Снорлакс Рождественский <xmassnorlax@outlook.com>
2026-05-25 21:10:01 +0300

    правки в модели

--- Models/Collection.cs
+++ Models/Collection.cs
@@ -1,17 +1,18 @@
-﻿namespace Documents.Models
+﻿namespace WriterApp.Models
 {
     public class Collection
     {
-        public Guid CollectionId { get;}
-        public string Name { get; set; }
-        // public int Amount { get; set; } = 0; наверно ненужный класс
-        public Dictionary<Guid,string> Books { get; set; } = new Dictionary<Guid,string>();
-
-        public Collection(string name)
+        public Collection(int id, string name)
         {
-            CollectionId = Guid.NewGuid();
+            Id = id;
             Name = name;
         }
 
+        public Collection() { }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; } = 0;
+        //public Dictionary<Guid,string> Books { get; set; } = new Dictionary<Guid,string>();
+        public List<Book> Books { get; set; } = new List<Book>();
     }
 }
