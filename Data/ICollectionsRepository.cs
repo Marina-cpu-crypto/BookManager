14a5e039de4cfc5083245d39dfc7f5dae83970f0
Marina-cpu-crypto <marina.khodova.06@mail.ru>
2026-06-18 20:10:04 +0300

    Update ICollectionsRepository.cs

--- Data/ICollectionsRepository.cs
+++ Data/ICollectionsRepository.cs
@@ -1,10 +1,14 @@
-﻿using Documents.Models;
+using WriterApp.Models;
 
-namespace Documents.Data
+namespace WriterApp.Data
 {
     public interface ICollectionsRepository
     {
+        List<UserData> GetAll();
         List<Collection> GetOne(Guid id);
+        Book TryGetById(Guid bookid);
+        //List<Book> GetBooks(Guid MainId);
+
         //void Remove(Book book, int ind);
         void ResetCollection();
         void ResaveUserData(List<Collection> collections);
