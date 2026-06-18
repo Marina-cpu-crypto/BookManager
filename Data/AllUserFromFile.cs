afbc2f200c109d261352295d99e2f5bd12f382ba
Marina-cpu-crypto <marina.khodova.06@mail.ru>
2026-06-18 20:12:16 +0300

    Add files via upload

---
+++ Data/AllUserFromFile.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Options;
+using System.Text.Json;
+using WriterApp.Data;
+using WriterApp.Models;
+using WriterApp.Controllers;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WriterApp.Data
+{
+    public class AllUsersFromFile : IUserRepository
+    {
+        public AllUsersFromFile() { }
+        public List<User> GetAll()
+        {
+            string jsonString = System.IO.File.ReadAllText("Data/users.json");
+            var users = JsonSerializer.Deserialize<List<User>>(jsonString) ?? new List<User>();
+
+            return users;
+        }
+        public void Save(Guid UserId)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+
+
+            UserData userData = new UserData(UserId);
+            string stringUD = System.IO.File.ReadAllText("Data/collections.json");
+            List<UserData> Userdatas = JsonSerializer.Deserialize<List<UserData>>(stringUD) ?? new List<UserData>();
+            Userdatas.Add(userData);
+            string newusersdata = JsonSerializer.Serialize(Userdatas, options);
+            System.IO.File.WriteAllText("Data/collections.json", newusersdata);
+
+            System.IO.File.WriteAllText("Data/MainId.txt", Convert.ToString(UserId));
+            Directory.CreateDirectory("Data/" + Convert.ToString(UserId));
+        }
+    }
+}
